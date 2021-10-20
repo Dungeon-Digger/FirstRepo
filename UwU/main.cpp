dc90907295b7d15a15016a3f5ab3031b0532e300
m1ne <oleksandr.babenko.25@gmail.com>
2021-10-20 20:18:05 +0300

    Added class Human --> Get's, Set's, Print, Default constructor

--- UwU/main.cpp
+++ UwU/main.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+class Human {
+	int age, weight;
+	string name;
+public:
+	Human() : Human("Default", 0, 0) {}
+	Human(string name, int age, int weight) : name(name), age(age), weight(weight) {}
+
+	void SetName(string name) { this->name = name; };
+	void SetAge(int age) { this->age = age; };
+	void SetWeight(int weight) { this->weight = weight; };
+
+	string GetName() { return name; }
+	int GetAge() { return age; }
+	int GetWeight() { return weight; }
+
+	void Print() { cout << "Human named " << name << " aged " << age << " with weight " << weight; }
+};
+
 void main() {
 	
 }
