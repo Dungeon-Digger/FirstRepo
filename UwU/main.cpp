3080407f81181557cb857c06bb9302a00dedfc3a
m1ne <oleksandr.babenko.25@gmail.com>
2021-10-20 20:40:34 +0300

    Added Student class inherited from Human. New fields in Student -> Average Marks variable with getter/setter, Group Name variable with getter/setter, Own Default Constructor

--- UwU/main.cpp
+++ UwU/main.cpp
@@ -17,9 +17,37 @@ public:
 	int GetAge() { return age; }
 	int GetWeight() { return weight; }
 
-	void Print() { cout << "Human named " << name << " aged " << age << " with weight " << weight; }
+	void Print() { cout << "Human named " << name << " aged " << age << " with weight " << weight << endl; }
+};
+
+class Student : public Human {
+	int avMark;
+	string groupName;
+public:
+	Student() : avMark(0), groupName("Default") {
+		SetName("Default");
+		SetAge(0);
+		SetWeight(0);
+	}
+	Student(string name, int age, int weight, string groupName, int avMark) : avMark(avMark), groupName(groupName) {
+		SetName(name);
+		SetAge(age);
+		SetWeight(weight);
+	}
+	Student(Human other, string groupName, int avMark): avMark(avMark), groupName(groupName) {
+		SetAge(other.GetAge());
+		SetName(other.GetName());
+		SetWeight(other.GetWeight());
+	}
+
+	void SetAvMark(int avMark) { this->avMark = avMark; };
+	void SetSetGroupName(string groupName) { this->groupName = groupName; };
+
+	int GetAvMark() { return avMark; }
+	string GetGroupName() { return groupName; }
 };
 
 void main() {
-	
+	Student s1;
+	s1.Print();
 }
